1e0da7f7e80816d243fb07065524afeb5d98ea6a
rebornwhu <rebornwhu@gmail.com>
2015-09-30 23:30:31 -0700

    Implement some methods

--- ios-view-switcher/SwitchingViewController.swift
+++ ios-view-switcher/SwitchingViewController.swift
@@ -14,6 +14,58 @@ class SwitchingViewController: UIViewController {
     private var yellowViewController: YellowViewController!
     
     @IBAction func switchViews(sender: UIBarButtonItem) {
+        if yellowViewController?.view.superview == nil {
+            if yellowViewController == nil {
+                yellowViewController = storyboard?.instantiateViewControllerWithIdentifier("Yellow") as! YellowViewController
+            }
+        }
+        else if blueViewController?.view.superview == nil {
+            if blueViewController == nil {
+                blueViewController = storyboard?.instantiateViewControllerWithIdentifier("Blue") as! BlueViewController
+            }
+        }
         
+        if blueViewController != nil && blueViewController!.view.superview != nil {
+            yellowViewController.view.frame = view.frame
+            switchViewController(from: blueViewController, to: yellowViewController)
+        }
+        else {
+            blueViewController.view.frame = view.frame
+            switchViewController(from: yellowViewController, to: blueViewController)
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        blueViewController = storyboard?.instantiateViewControllerWithIdentifier("Blue") as! BlueViewController
+        blueViewController.view.frame = view.frame
+        switchViewController(from: nil, to: blueViewController)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+        if blueViewController != nil && blueViewController!.view.superview == nil {
+            blueViewController = nil
+        }
+        if yellowViewController != nil
+            && yellowViewController!.view.superview == nil {
+                yellowViewController = nil
+        }
+    }
+    
+    private func switchViewController(from fromVc:UIViewController?, to toVC:UIViewController?) {
+        if fromVc != nil {
+            fromVc!.willMoveToParentViewController(nil)
+            fromVc!.view.removeFromSuperview()
+            fromVc!.removeFromParentViewController()
+        }
+        
+        if toVC != nil {
+            self.addChildViewController(toVC!)
+            self.view.insertSubview(toVC!.view, atIndex: 0)
+            toVC!.didMoveToParentViewController(self)
+        }
     }
 }
