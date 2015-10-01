a8947aa2fae2b20cd7b2129c11d0f521cbb59e33
rebornwhu <rebornwhu@gmail.com>
2015-09-30 22:06:13 -0700

    First commit

---
+++ ios-view-switcher/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-view-switcher
+//
+//  Created by Xiao Lu on 9/30/15.
+//  Copyright © 2015 Xiao Lu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
