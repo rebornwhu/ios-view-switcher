5e914422afa01834adab6d2f74a29e0fa74d02a4
rebornwhu <rebornwhu@gmail.com>
2015-09-30 22:32:09 -0700

    Create project skeleton

---
+++ ios-view-switcher/SwitchingViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SwitchingViewController.swift
+//  ios-view-switcher
+//
+//  Created by Xiao Lu on 9/30/15.
+//  Copyright © 2015 Xiao Lu. All rights reserved.
+//
+
+import UIKit
+
+class SwitchingViewController: UIViewController {
+
+    private var blueViewController: BlueViewController!
+    private var yellowViewController: YellowViewController!
+    
+    @IBAction func switchViews(sender: UIBarButtonItem) {
+        
+    }
+}
